38a88961d360a2e06b98f1d97cba94ed75dfa487
Reza <felegarireza72@gmail.com>
2020-03-02 15:02:58 +0330

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,14 +2,17 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"/assets/fonts/iran_yekan_mob_light_fa_num.ttf": "abb72bff27e98f5b815a74816a63544b",
+"/assets/fonts/iran_yekan_mob_reg_fa_num.ttf": "d507fb9ef76a6aa4d0b42cf37dd64476",
+"/assets/AssetManifest.json": "08423463951db30e326c7911cd5e6727",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/FontManifest.json": "0fa4596848cf1ae456bcb6adc62e2243",
+"/assets/images/logo.png": "19d11a0e42c1a4f624437a88b4d77ac5",
 "/index.html": "89785851368d424be3d0f8b214d40ab5",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/main.dart.js": "52c81c3e4e93630e8b831dd1fcdf742b",
+"/main.dart.js": "62ebb7c702c6e4aab2e16688bdef6491",
 "/manifest.json": "da6bc2cf224a2da53970b0eff3b9dfdf"
 };
 
